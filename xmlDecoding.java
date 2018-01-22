b004d23ec21ae0c6fa62153fbcd43cf494e2494b
Cedricboisselrichard <cedricboisselrichard@gmail.com>
2018-01-22 18:09:43 +0100

    modif decoding
    façon sale mais ça fonctionne

--- xmlDecoding.java
+++ xmlDecoding.java
@@ -38,89 +38,52 @@ public class xmlDecoding
 			
 
 
-			final NodeList ref = racine.getElementsByTagName("ref");
-			final int nbNoeuds = ref.getLength();
-			System.out.println(nbNoeuds);
+			final NodeList ref = racine.getElementsByTagName("ref");			
+			final Element n1 = (Element) ref.item(0);
+			//System.out.println(bla.getNodeName());
 
-			for (int j = 0; j<nbNoeuds; j++)
-			{	
-				final Element bla = (Element) ref.item(j);
-				System.out.println(bla.getNodeName());
-
-				final NodeList date = bla.getElementsByTagName("date");
-				final Element hum = (Element) date.item(0);
-				System.out.println(hum.getNodeName() + " : " + hum.getTextContent());
+			final NodeList date = n1.getElementsByTagName("date");
+			final Element dateElem = (Element) date.item(0);
+			System.out.println(dateElem.getNodeName() + " : " + dateElem.getTextContent());
 
 
-				final NodeList station = bla.getElementsByTagName("station");
-				final Element hum2 = (Element) station.item(0);
-				System.out.println(hum2.getNodeName() + " : " + hum2.getTextContent());
+			final NodeList station = n1.getElementsByTagName("station");
+			final Element stationElem = (Element) station.item(0);
+			System.out.println(stationElem.getNodeName() + " : " + stationElem.getTextContent());
 
-				/*switch(j)
-				{
-					case 0:
-						Element date = (Element)ref.item(j);
-						dateStr = date.getTextContent();
-						System.out.println(dateStr.length());
-						break;
-					case 1:
-						Element station = (Element)ref.item(j);
-						stationStr = station.getTextContent();
-						System.out.println(stationStr.length());
-						break;
-					case 2:
-						Element program = (Element)ref.item(j);
-						programStr = program.getTextContent();
-						System.out.println(programStr.length());
-						break;
-				}*/
-			}
+			final NodeList program = n1.getElementsByTagName("program");
+			final Element programElem = (Element) program.item(0);
+			System.out.println(programElem.getNodeName() + " : " + programElem.getTextContent());
+
+
+			final NodeList system = racine.getElementsByTagName("system");			
+			final Element n2 = (Element) system.item(0);
 			
-			final NodeList system = racine.getElementsByTagName("system");
-			final int nbNoeuds2 = system.getLength();
+			final NodeList systemStatus = n2.getElementsByTagName("status");
+			final Element systemStatusElem = (Element) systemStatus.item(0);
+			System.out.println(systemStatusElem.getNodeName() + " : " + systemStatusElem.getTextContent());
 
-			for (int i = 0; i<nbNoeuds2; i++)
-			{	
-				/*switch(i)
-				{
-					case 0:
-						Element systemStatus = (Element)system.item(i);
-						systemStatusStr = systemStatus.getTextContent();
-						break;
-				}*/
-			}
+			
 
-			final NodeList process = racine.getElementsByTagName("process");	
-			final int nbNoeuds3 = process.getLength();
+			final NodeList process = racine.getElementsByTagName("process");
+			final Element n3 = (Element) process.item(0);
+			
+			final NodeList processStatus = n3.getElementsByTagName("status");
+			final Element processStatusElem = (Element) processStatus.item(0);
+			System.out.println(processStatusElem.getNodeName() + " : " + processStatusElem.getTextContent());
 
-			for (int l = 0; l<nbNoeuds3; l++)
-			{	
-				/*switch(l)
-				{
-					case 0:
-						Element processStatus = (Element)process.item(l);
-						processStatusStr = processStatus.getTextContent();
-						break;
-					case 1:
-						Element nogo = (Element)process.item(l);
-						nogoStr = nogo.getTextContent();
-						break;
-				}*/
-			}
+			final NodeList nogo = n3.getElementsByTagName("nogo");
+			final Element nogoElem = (Element) nogo.item(0);
+			System.out.println(nogoElem.getNodeName() + " : " + nogoElem.getTextContent());
+	
 
 			final NodeList function = racine.getElementsByTagName("function");
-			final int nbNoeuds4 = function.getLength();
+			final Element n4 = (Element) function.item(0);
 
-			for (int k = 0; k<nbNoeuds4; k++)
-			{	
-				/*switch(k)
-				{
-					case 0:
-						Element session = (Element)function.item(k);
-						sessionStr = session.getTextContent();
-						break;
-				}*/
-			}
+			final NodeList session = n4.getElementsByTagName("session");
+			final Element sessionElem = (Element) session.item(0);
+			System.out.println(sessionElem.getNodeName() + " : " + sessionElem.getTextContent());
+
 
 			//Data data = new Data(dateStr,stationStr,programStr,systemStatusStr,processStatusStr,nogoStr,sessionStr);							
 			//System.out.println(data);
